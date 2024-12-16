f9576ed372f33230cbc644320498c2113d0e1325
Jaromir Sheyh <tyrikaltsheyhog@gmail.com>
2024-12-16 21:21:17 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/tyriksheyh4567.github.io",
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/tyriksheyh4567.github.io/yaros-website/",
 };
